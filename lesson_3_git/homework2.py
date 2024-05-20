85808e5d8a981fb2f890d1737dde78c27e51d8fb
Ваше Имя <shterbax@gmail.com>
2024-05-20 16:44:49 +0300

    New push

--- lesson_3_git/homework2.py
+++ lesson_3_git/homework2.py
@@ -2,7 +2,10 @@ li = [0, 1, 7, 2, 4, 8]
 
 sum = 0
 
-for x in li[::2]:
-    sum += x
+if len(li) != 0:
+    for x in li[::2]:
+        sum += x
 
-print(sum * li[-1])
+    print(sum * li[-1])
+else:
+    print(0)
